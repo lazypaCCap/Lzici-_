05c47420da8c2885e7bf33aa0df66bb35d2f1d23
lazypaCCap <w_xu_you@126.com>
2026-07-30 17:34:25 +0800

    Add files via upload

---
+++ LziciCUI.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python3
+from textual.app import App
+from textual.widgets import Input, Button, RichLog, Header, Footer, Button
+from textual.containers import VerticalGroup, HorizontalGroup
+import Lzicicore
+import sys
+侧边栏开启 = False
+core = Lzicicore.core()
+if(core.gety() == True):
+    引导 = True
+else:
+    引导=False
+class Cbl(VerticalGroup):
+    def on_mount(self):
+        self.styles.width = 0
+        self.cblzt()
+        
+    def cblzt(self):
+        self.mount(Button("历史记录<ctrl+l>", id="history", variant="default"))
+        self.mount(Button("设置<ctrl+s>", id="settings", variant="default"))
+        self.mount(Button("退出程序<ctrl+e>", id="quit", variant="default"))
+        
+    def on_button_pressed(self, event):
+        bid = event.button.id
+        if bid == "quit":
+            self.app.exit()
+        elif bid == "settings":
+            self.app.action_设置()
+        elif bid == "history":
+            self.app.action_历史()
+        else:
+            sys.exit(1)
+class qd(App):
+    BINDINGS = [("ctrl+c", "侧边栏", ""),
+    ("ctrl+e", "退出", "退出"),
+    ("ctrl+s", "设置", ""),
+    ("ctrl+l", "历史",""),
+    ("ctrl+q", "nexit","")]
+    def __init__(self):
+         super().__init__()
+         global 引导
+         global useing
+         useing = False
+         if (引导 == True):
+             ...
+             
+         
+    def action_侧边栏(self):
+        # cbll这个变量在本类的compose()被定义
+        global 侧边栏开启
+        global cbll
+        self.log("cbll:", cbll)
+        if(侧边栏开启 == True):
+            侧边栏开启 = False
+            cbll.styles.width = 0
+        else:
+            侧边栏开启 = True
+            cbll.styles.width = 30
+            self.notify(f"如要退出，请使用ctrl+e(If you want to exit,use ctrl+e)")
+    def action_退出(self):
+        self.exit()
+    def action_设置(self):
+        ...
+    def action_历史(self):
+        ...
+    def compose(self):
+        global cbll
+        global main
+        global maini
+        global maint
+        cbll = Cbl()
+        self.log("cbll:", cbll)
+        with HorizontalGroup():
+            yield cbll
+            with VerticalGroup():
+                maint = RichLog()
+                maint.styles.height = "1fr"
+                yield maint
+                #maini=main+input
+                #maint=main+text
+                #mainr=main+right
+                maini = Input(placeholder="输入消息,Enter发送,此版本暂不支持换行,使用Ctrl+C来开启/关闭侧边栏,Ctrl+E来退出,暂不支持上下文和流式输出")
+                maini.styles.height = 5
+                yield maini
+    def action_nexit(self):
+        self.notify(f"如要退出，请使用ctrl+e(If you want to exit,use ctrl+e)")
+    def on_input_submitted(self, event: Input.Submitted):
+        global useing
+        global maini
+        global maint
+        inp = event.value
+        if useing == True:
+            return
+        elif (inp == ""):
+            self.log("内容不能为空")
+            return
+        elif(inp == " "):
+            self.log("内容不能为空")
+            maini.value = ""
+            return
+        maini.value = ""
+        maint.write(f"你：{inp}")
+        useing = True
+        aiout = core.askn(inp)
+        if not (aiout == ""):
+            maint.write(f"AI：{aiout}")
+        useing = False
+qd().run()
---
+++ Lzicicore.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import subprocess
+import os
+import json
+#定义目录
+目录 = os.path.expanduser("~/.config/Lzici")
+设置文件 = os.path.expanduser("~/.config/Lzici/设置.json")
+#判断，创建，写默认
+if os.path.isdir(目录):
+    引导 = False
+else:
+    os.makedirs(目录, exist_ok=True)
+    日志目录 = os.path.expanduser("~/.config/Lzici/log")
+    os.makedirs(日志目录, exist_ok=True)
+    默认数据 = {"温度": "0.6","重复惩罚": "1.1","最大生成长度": "8192","上下文长度": "16384","top_k": "40","top_p": "0.9","停止词": "","存活时间": "120"}
+    with open(设置文件, "w", encoding="utf-8") as f:
+        json.dump(默认数据, f, indent=4, ensure_ascii=False)
+    引导 = True
+#读设置
+with open(设置文件, "r", encoding="utf-8") as f:
+    数据 = json.load(f)
+#写变量
+globals().update(数据)
+class core:
+    def gety(self):
+        return 引导
+    def getm(self,模型名):
+        global 数据
+        数据["模型名"] = 模型名
+        with open(设置文件, "w", encoding="utf-8") as f:
+            json.dump(数据, f, indent=4, ensure_ascii=False)
+    def askn(self,问题):
+        种子 = str(int.from_bytes(os.urandom(4), "big") % 1000000 + 1)
+        结果 = subprocess.run(
+            ["./CLI.py", 问题, 温度,重复惩罚,种子,最大生成长度,上下文长度,top_k,top_p,停止词,存活时间,模型名,"False"],
+            capture_output=True,
+            text=True
+        )
+        if 结果.stdout == None:
+            错误="error"
+            return 错误
+        else:
+            return 结果.stdout
+    
